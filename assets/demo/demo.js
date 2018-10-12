ee1e89e3bda570c1ce8a621f6ad47c34df5d1ec9
Dragos <dragosct10@gmail.com>
2018-10-12 14:37:36 +0300

    nud v1.2.0

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -443,23 +443,5 @@ demo = {
 
         // To add the marker to the map, call setMap();
         marker.setMap(map);
-    },
-
-	showNotification: function(from, align){
-    	color = 'primary';
-
-    	$.notify({
-        	icon: "now-ui-icons ui-1_bell-53",
-        	message: "Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer."
-
-        },{
-            type: color,
-            timer: 8000,
-            placement: {
-                from: from,
-                align: align
-            }
-        });
-	}
-
+    }
 };
